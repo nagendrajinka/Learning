0f4469dd755b4659781f84f01029ac303608b946
nagendrajinka <nagendrababu.jinka@gmail.com>
2018-07-09 04:00:47 -0700

    Palindrome Program

--- src/main/java/training/weekTwo/Pallindrome.java
+++ src/main/java/training/weekTwo/Pallindrome.java
@@ -1,4 +1,21 @@
 package training.weekTwo;
 
+/**
+ * Pallindrome program
+ */
+
 public class Pallindrome {
+
+    public static boolean isPallindrome(String name) {
+        for(int i=0; i<name.length(); i++) {
+            if (name.charAt(i) != name.charAt((name.length()-1)-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPallindrome("madam"));
+    }
 }
