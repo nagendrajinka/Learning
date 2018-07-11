75ec80c4d0de087f5f201e1f5bad99d0fd5ae87e
nagendrajinka <nagendrababu.jinka@gmail.com>
2018-07-11 02:16:11 -0700

    Two Strings

---
+++ src/main/java/training/weekTwo/JoiningTwoStrings.java
@@ -0,0 +1,26 @@
+package training.weekTwo;
+
+public class JoiningTwoStrings {
+    public static String TwoStrings() {
+        String s1 = "abcd";
+        String s2 = "efgh";
+        String s3 = " ";
+        int len = s1.length() + s2.length();
+        for (int i = 0; i < len; i++) {
+            if (i % 2 == 0) {
+                s3 += s1.charAt(i / 2);
+            } else {
+                s3 += s2.charAt((i - 1) / 2);
+            }
+        }
+        System.out.println(s3);
+        return  s3;
+    }
+
+    public static void main(String[] args) {
+        TwoStrings();
+    }
+}
+
+
+
