0158865ff5e2ffeffda807a8e265be67a6590d53
nagendrajinka <nagendrababu.jinka@gmail.com>
2018-07-11 03:42:21 -0700

    Program based on Star

--- src/main/java/training/weekTwo/IncrementStarsReverse.java
+++ src/main/java/training/weekTwo/IncrementStarsReverse.java
@@ -10,9 +10,9 @@ public class IncrementStarsReverse {
         for (int i = 5; i >= 1; i--) {
             for (int j = 1; j <= 5; j++) {
                 if (j >= i) {
-                    System.out.print("*");
+                    System.out.print("* ");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.print("\n");
